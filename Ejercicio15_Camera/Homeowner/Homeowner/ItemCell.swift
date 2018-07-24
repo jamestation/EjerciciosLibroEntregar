6a04fbd3b4941e4e3c3d77d0d3eb195cf2be19f9
Jaime ISLAS <jaimeislas@Jaimes-MacBook-Pro.local>
2018-07-23 19:17:00 -0500

    Se agrega ejercicio 15 libro

---
+++ Ejercicio15_Camera/Homeowner/Homeowner/ItemCell.swift
@@ -0,0 +1,25 @@
+//  ItemCell.swift
+//  Homeowner
+//
+//  Created by Jaime ISLAS on 21/07/18.
+//  Copyright © 2018 Jaime ISLAS. All rights reserved.
+//
+
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+}
